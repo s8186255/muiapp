18eeaee3b6580f35f93e4471c125d568dfeae63a
suaq <s8186255@163.com>
2015-07-28 19:30:15 +0800

    优化

---
+++ myapp/js/contacts.js
@@ -0,0 +1,42 @@
+function showContacts() {
+	//添加plus事件处理
+	document.addEventListener("plusready", function() {
+		find();
+	}, false);
+
+
+}
+
+function find() {
+	plus.contacts.getAddressBook(
+		plus.contacts.ADDRESSBOOK_PHONE,
+		function(addressbook) {
+			contactStr = '';
+			addressbook.find(
+				["displayName", "phoneNumbers"],
+				function(contacts) {
+					length = contacts.length;
+					for (var i = 0; i < length; i++) {
+						try {
+							contactStr += '<li  class="mui-table-view-cell mui-group-list-item mui-checkbox mui-left" onclick="handle(this)"><input type="checkbox"/>' + contacts[i].displayName + "|" + contacts[i].phoneNumbers[0].value + '</li>';
+						} catch (e) {
+							continue;
+						}
+					};
+					document.getElementById('phoneList').innerHTML = contactStr + length.toString();
+				},
+				function() {
+					alert("error");
+				}, {
+					multiple: true
+				});
+		},
+		function(e) {
+			alert("Get address book failed: " + e.message);
+		});
+};
+
+function handle(item) {
+	//if(item.querySelector("input").checked
+	//alert(item.querySelector("input").checked);
+};
--- myapp/js/login.js
+++ myapp/js/login.js
@@ -24,24 +24,53 @@
 //};
 
 function openNewPage(tapId, url) {
-			document.getElementById(tapId).addEventListener('tap', function() {
-				//打开关于页面
-				console.log(localStorage.token);
-				console.log(plus.storage.getItem('token'));
-				if (localStorage.getItem("token") == '') {
-					var webview = mui.openWindow({
-						//id: tapId,
-						url: url,
-						styles: {
-							//top: '45px',
-							//bottom: '50px',
-							bounce: 'vertical',
-						},
-					});
-				} else {
-					mui.openWindow({
-						url: "login.html"
-					})
-				}
+	document.getElementById(tapId).addEventListener('tap', function() {
+		//打开关于页面
+		if (isLogin()) {
+			var webview = mui.openWindow({
+				url: url,
+				styles: {
+					top: '45px',
+					bottom: '50px',
+					bounce: 'vertical',
+				},
 			});
+		} else {
+			mui.openWindow({
+				url: "login.html"
+			})
 		}
+	});
+}
+
+function isLogin() {
+	if (localStorage.getItem("token") == null) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function checkUserLogin(token, url) {
+	mui.ajax(
+		url, {
+			type: 'post',
+			data: {
+				token: token
+			},
+
+			success: function(data) {
+				return
+			},
+			error: function(data) {
+				return false;
+			}
+		})
+
+}
+
+function login() {
+	mui.openWindow({
+		url: "login.html"
+	})
+}
